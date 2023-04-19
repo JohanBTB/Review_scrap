59b6e7d360d52e2fa29b271c521bfd41e89c144e
JohanBTB <johan10-2001@hotmail.com>
2023-04-18 23:00:17 -0500

    Well organized

--- main_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from user import User
-
-user = User("RogerEbert.com", "https://www.metacritic.com/publication/rogerebertcom?page=")
-
-user.get_users()
-
-user.write_json()
